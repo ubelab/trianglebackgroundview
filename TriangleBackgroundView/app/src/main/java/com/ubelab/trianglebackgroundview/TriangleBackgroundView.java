5e5122cca194e7c61ee8caf276aad213688533b2
Marco Uberti <ube.labs@gmail.com>
2015-03-14 13:24:57 +0100

    First commit

---
+++ TriangleBackgroundView/app/src/main/java/com/ubelab/trianglebackgroundview/TriangleBackgroundView.java
@@ -0,0 +1,71 @@
+package com.ubelab.trianglebackgroundview;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * Created by Marco on 03/03/15.
+ */
+public class TriangleBackgroundView extends View {
+
+    Paint paint;
+    Paint bgPaint;
+
+    public TriangleBackgroundView(Context context) {
+        super(context);
+        init();
+    }
+
+    public TriangleBackgroundView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public TriangleBackgroundView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setStrokeWidth(1);
+        paint.setAntiAlias(true);
+        paint.setStrokeCap(Paint.Cap.SQUARE);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GRAY);
+
+        bgPaint= new Paint();
+        bgPaint.setStyle(Paint.Style.FILL);
+        bgPaint.setColor(Color.WHITE);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int h = getMeasuredHeight();
+        int w = getMeasuredWidth();
+
+        canvas.drawRect(0,0,w,h,bgPaint);
+
+        Path path = new Path();
+        path.moveTo(0, h);
+        path.lineTo(w, h);
+        path.lineTo(0, 0);
+        path.lineTo(0, h);
+        path.close();
+        canvas.drawPath(path,paint);
+    }
+}
